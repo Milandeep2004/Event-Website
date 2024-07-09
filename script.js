6a9709f116e0d0e327d49043f9cb517e9bd6bf97
Milandeep2004 <milandeep2004@gmail.com>
2024-07-09 21:34:46 +0530

    event website

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('event1').addEventListener('click', () => {
+        window.location.href = 'event1.html';
+    });
+
+    document.getElementById('event2').addEventListener('click', () => {
+        window.location.href = 'event2.html';
+    });
+});
